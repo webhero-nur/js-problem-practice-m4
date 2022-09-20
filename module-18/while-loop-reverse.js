1cccb6faec72c5fd149231bf5c03763b73143933
webhero-nur <nurealom522@gmail..com>
2022-09-20 20:02:16 +0600

    Module 18, Problem 3 (while loop reverse) solved

---
+++ module-18/while-loop-reverse.js
@@ -0,0 +1,16 @@
+const dailyTasks = [
+    "What is your daily tasks?",
+    "1. Unlock a module at 9'o clock",
+    "2. Quickly watch the tutorial video and start practice",
+    "3. Take notes while watching the video",
+    "4. Try the whole module by yourself after finishing the module",
+    "5. If you are unable to understand (try to think but here a condition is applied), join the support session"
+];
+
+let i = 10;
+while (i >= 0) {
+    if (i < dailyTasks.length) {
+        console.log(dailyTasks[i]);
+    }
+    i = i - 1;
+}

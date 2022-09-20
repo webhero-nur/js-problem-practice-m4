742a6e389532b5bad2feed96882aa0a3b7e649f7
webhero-nur <nurealom522@gmail..com>
2022-09-20 20:03:42 +0600

    Module 18, Problem 4 (for loop reverse) solved

---
+++ module-18/for-loop-reverse.js
@@ -0,0 +1,14 @@
+const dailyTasks = [
+    "What is your daily tasks?",
+    "1. Unlock a module at 9'o clock",
+    "2. Quickly watch the tutorial video and start practice",
+    "3. Take notes while watching the video",
+    "4. Try the whole module by yourself after finishing the module",
+    "5. If you are unable to understand (try to think but here a condition is applied), join the support session"
+];
+
+for (let i = 10; i >= 0; i--) {
+    if (i < dailyTasks.length) {
+        console.log(dailyTasks[i]);
+    }
+}

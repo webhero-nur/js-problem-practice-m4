adbf10d4482ced3fa79a5d5e4931cbeb1c408c94
webhero-nur <nurealom522@gmail..com>
2022-09-20 18:37:41 +0600

    Module 16 All practice problems solved. Finding error in variable declaration.

---
+++ module-16/error-find.js
@@ -0,0 +1,56 @@
+Var price = 33
+/**
+ * When declaring the variable, it must be declared using library keyword (var, let, const).
+ * After using var, we are still gatting error. JavaScript is a case sensitive language. Means, "Var" and "var" are not the same thing.
+ * As we need to write library key as it is, we are getting an error as JS is a case sensitive language.
+ * Another mistake is, we didn't close/end the line. To end the line we need to add a semicolon (;) in the end.
+ * 
+ * Correct Answer is:                   var price = 33;
+ */
+
+var name - Shabana
+/**
+ * There must me an assign simbol which is equal (=) to assign a value to a variable.
+ * We cannot write only minas (-) simbol to assign value to a variable.
+ * To insert a string value to a variable, coutation ("" or '') mark is a must.
+ * Last mistake is, we didn't close/end the line. To end the line we need to add a semicolon (;) in the end.
+ * 
+ * Correct Answer is:                   var name = 'Shabana';
+ */
+
+var boxName = 'Colola;
+/**
+ * Here the mistake is we didn't close the coutation mark. We must end the string using coutation mark 
+ * But to remember, we need to end the coutation mark by the same mark we used before the string started.
+ * If the coutation start with single coutation ending must be single coutation and if start with double, ending must be the same.
+ * 
+ * Correct Answer is:                   var boxName = 'Colola';
+ */
+
+var 88_price = 88;
+/**
+ * We cannot start a variable name with a number. First letter of a variable must be a character.
+ * 
+ * Correct Answer is:                   var _price = 88;    or,     var price88 = 88;   or,     var price_ = 88;  or, var price_88 = 88;
+ */
+
+var enum = -1;
+/**
+ * All conditions of variable naming convention are fullfiled here except one condition.
+ * Variable name cannot be reserve words. enum is a reserve word for JS. So we cannot declare a variable named enum.
+ * 
+ * Correct Answer is:                   var eNum = -1;      or,     var enum1 = -1;     or,     var enum_ = -1;      etc.
+ */
+var _$box'78 = 'Monika';
+/**
+ * We cannot use special character in a variable name. Every letter of a variable must be a character.
+ * We can use number too in a variable just not at the beginning/start.
+ * 
+ * Correct Answer is:                   var _box78 = 'Monika';
+ */
+var home-address = "kochu khet";
+/**
+ * We only can use underscore (_) special character in a variable name. Hyphen or ther special characters are not allowed in a variable name.
+ * 
+ * Correct Answer is:                   var home_address = "kochu khet";    or,     homeAddress = "kochu khet";
+ */
